41487465a043e9f9e5b2fd2de0be0f4920639ec4
pineapplemanxd <117184681+pineapplemanxd@users.noreply.github.com>
2022-11-07 18:36:17 +0100

    Update sqaure-create.py

--- sqaure-create.py
+++ sqaure-create.py
@@ -1,1 +1,32 @@
+import requests
+content = "⬜ "
+################################################################################
+link = "link to a channel here"
+auth = "your auth token here"
 
+###########################################################################
+x = int(input("set x: "))
+y = int(input("set y: "))
+cont = input("what sign to use on making a square(leave blank to defult)")
+if x > 7:
+    print("x is to big(max 7)")
+if cont == "":
+    if x < 8:
+        for s in range (y):
+            payload = {
+            'content': (content*x)
+}
+            header = {
+                'authorization': auth
+}
+            r = requests.post(link, data=payload, headers = header )
+else:
+    if x < 8:
+        for s in range (y):
+            payload = {
+            'content': (cont*x)
+}
+            header = {
+                'authorization': auth
+}
+            r = requests.post(link, data=payload, headers = header )
